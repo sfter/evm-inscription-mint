fa706f3ceb637a6fad5592104fa6cfe02167fd35
stonegate <stonega@users.noreply.github.com>
2024-01-21 05:46:22 +0000

    使用Buffer转为16进制

--- index.js
+++ index.js
@@ -53,13 +53,7 @@ async function checkAvaible(tokenJson){
 
 // 转成16进制
 const convertToHexa = (str = '') =>{
-   const res = [];
-   const { length: len } = str;
-   for (let n = 0, l = len; n < l; n ++) {
-      const hex = Number(str.charCodeAt(n)).toString(16);
-      res.push(hex);
-   };
-   return `0x${res.join('')}`;
+  return `0x${Buffer.from(str).toString('hex')}`
 }
 
 // 获取当前账户的 nonce
